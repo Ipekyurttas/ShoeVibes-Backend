1038d6f655c47b64925a68c9c642b1203e8823a2
yeşim <130006142+yesimkaya100619@users.noreply.github.com>
2025-03-09 23:15:47 +0300

    İlk proje yüklemesi

---
+++ start/src/main/java/com/ShoeVibes/entity/Review.java
@@ -0,0 +1,87 @@
+package com.ShoeVibes.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reviewerName;
+    private String comment;
+    private Integer rating;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public Review() {
+    }
+
+    public Review(Long id, String reviewerName, String comment, Integer rating, Product product, LocalDateTime createdAt) {
+        this.id = id;
+        this.reviewerName = reviewerName;
+        this.comment = comment;
+        this.rating = rating;
+        this.product = product;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReviewerName() {
+        return reviewerName;
+    }
+
+    public void setReviewerName(String reviewerName) {
+        this.reviewerName = reviewerName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
